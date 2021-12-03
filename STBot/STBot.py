46362eac6296e7854c14ef55e9559ccb4234c940
Artemiy <46986544+c4kef@users.noreply.github.com>
2021-12-03 16:59:35 +0300

    Update

--- STBot/STBot.py
+++ STBot/STBot.py
@@ -1,10 +1,12 @@
 import telebot
+import clr
+
+clr.AddReference("D:\Build\Debug\STLib.dll")
+from STLib import Class1
 from telebot import apihelper
 
 token = '2045425760:AAHossESgiQy5DAgtnUse3vDFTyTgkYufGk'
-
 bot = telebot.TeleBot(token)
-
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -12,18 +14,12 @@ def start_message(message):
     keyboard.row('/test')
     bot.send_message(message.chat.id, 'Привет!', reply_markup=keyboard)
 
-
-
-
 @bot.message_handler(commands=['test'])
 def start_message(message):
-    a = ['1. Задачи', '2. Задачи', '3. Задачи']
     markup = telebot.types.InlineKeyboardMarkup()
-    for x in a:
+    for x in Class1.GetStrings():
         markup.add(telebot.types.InlineKeyboardButton(text = x, callback_data = x))
     bot.send_message(message.chat.id, text="Добрый день, выберите задачу:", reply_markup=markup)
-    
-
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
@@ -34,14 +30,6 @@ def query_handler(call):
     answer = 'Вы троечник!'
     bot.send_message(call.message.chat.id, answer)
     bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
-
-
-
-
-
-
-        
-    
 
 bot.polling()
     
