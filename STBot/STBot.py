0c38f7304b086a7176a0f18d084dda362e781e00
gfgfhvfvfhf41 <gfgfhvfvfhf41@gmail.com>
2021-12-03 16:51:11 +0300

    ты пидор

--- STBot/STBot.py
+++ STBot/STBot.py
@@ -1,10 +1,49 @@
-import clr
+import telebot
+from telebot import apihelper
 
-def printTick(answer):
-  Class1.Write(answer)
+token = '2045425760:AAHossESgiQy5DAgtnUse3vDFTyTgkYufGk'
 
-clr.AddReference("D:\Build\Debug\STLib.dll")
-from STLib import Class1
-Class1.Write("Hello world!")
-Class1.Init()
-Class1.testEvent += printTick
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    keyboard = telebot.types.ReplyKeyboardMarkup(True)
+    keyboard.row('/test')
+    bot.send_message(message.chat.id, 'Привет!', reply_markup=keyboard)
+
+
+
+
+@bot.message_handler(commands=['test'])
+def start_message(message):
+    a = ['1. Задачи', '2. Задачи', '3. Задачи']
+    markup = telebot.types.InlineKeyboardMarkup()
+    for x in a:
+        markup.add(telebot.types.InlineKeyboardButton(text = x, callback_data = x))
+    bot.send_message(message.chat.id, text="Добрый день, выберите задачу:", reply_markup=markup)
+    
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+    b = ['1. Задачи', '2. Задачи', '3. Задачи']
+    bot.answer_callback_query(callback_query_id=call.id, text='Спасибо за честный ответ!')
+    print(call.data)
+
+    answer = 'Вы троечник!'
+    bot.send_message(call.message.chat.id, answer)
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+
+
+
+
+
+
+        
+    
+
+bot.polling()
+    
+
+
