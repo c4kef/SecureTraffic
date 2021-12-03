9b7fb564c5469250dfc78f6d165d3e5e134b9209
gfgfhvfvfhf41 <gfgfhvfvfhf41@gmail.com>
2021-12-03 19:55:08 +0300

    ты лоx)

--- STBot/STBot.py
+++ STBot/STBot.py
@@ -1,37 +1,42 @@
-import telebot
-import clr
+from const import token, n, s1, s3
+from imports import telebot, clr, Class1, telebot, apihelper
 
-clr.AddReference("D:\Build\Debug\STLib.dll")
-from STLib import Class1
-from telebot import apihelper
 
-token = '2045425760:AAHossESgiQy5DAgtnUse3vDFTyTgkYufGk'
+
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start'])
+
+
+@bot.message_handler(commands=[s1])
 def start_message(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('/test')
-    bot.send_message(message.chat.id, 'Привет!', reply_markup=keyboard)
+    keyboard.row(n)
+    bot.send_message(message.chat.id, 'Привет', reply_markup = keyboard)
+    user_id = message.from_user.id
+    print(user_id)
 
-@bot.message_handler(commands=['test'])
+
+
+@bot.message_handler(commands = [s3])
 def start_message(message):
+    bot.delete_message(message.chat.id, message.message_id)
     markup = telebot.types.InlineKeyboardMarkup()
-    for x in Class1.GetStrings():
-        markup.add(telebot.types.InlineKeyboardButton(text = x, callback_data = x))
-    bot.send_message(message.chat.id, text="Добрый день, выберите задачу:", reply_markup=markup)
+    a = 1
+    for x in Class1.GetQuestions():
+        markup.add(telebot.types.InlineKeyboardButton(text = str(a) + '. ' + x, callback_data = x))
+        a+=1
+    bot.send_message(message.chat.id, text = "Добрый день, выберите задачу:", reply_markup = markup)
+    
 
-@bot.callback_query_handler(func=lambda call: True)
+ 
+@bot.callback_query_handler(func = lambda call: True)
 def query_handler(call):
-    b = ['1. Задачи', '2. Задачи', '3. Задачи']
-    bot.answer_callback_query(callback_query_id=call.id, text='Спасибо за честный ответ!')
+    bot.answer_callback_query(callback_query_id = call.id, text = 'Спасибо за честный ответ!')
     print(call.data)
-
-    answer = 'Вы троечник!'
+    answer = Class1.GetAnswer(call.data)
     bot.send_message(call.message.chat.id, answer)
     bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 
-bot.polling()
     
 
-
+bot.polling()
---
+++ STBot/const.py
@@ -0,0 +1,7 @@
+token = '2045425760:AAHossESgiQy5DAgtnUse3vDFTyTgkYufGk'
+n = '/test'
+s1 = 'start'
+s3 = 'test'
+m = 'reg'
+
+
